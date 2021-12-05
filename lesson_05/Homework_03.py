89780f9fc7308a1fcdfe2de2a5075cda465bd55f
Kavalevski Yuri <kavalevskiyuri@gmail.com>
2021-12-05 23:17:54 +0300

    Change for Homework_03 in lesson_05

--- lesson_05/Homework_03.py
+++ lesson_05/Homework_03.py
@@ -1,13 +1,18 @@
-"""аписать функцию, которая используя модуль requests скачивает файл сохраняет
-его на файловой системе, функция имеет два параметра: ссылка на файл и имя на
-файловой системе. В качестве примера ссылки на файл можно использовать лицензию из
-ГитХаба из вашего репозитория: https://raw.githubusercontent.com/manti-by/lessons/master/LICENSE"""
 
+
+"""
+Написать функцию, которая используя модуль requests скачивает файл сохраняет его на файловой системе, функция имеет два
+параметра: ссылка на файл и имя на файловой системе. В качестве примера ссылки на файл можно использовать лицензию из
+ГитХаба из вашего репозитория: https://raw.githubusercontent.com/manti-by/lessons/master/LICENSE
+"""
 import requests
 
-def my_func(save, link):
-    file = open(save,"wb")
-    opened = requests.get(link)
-    file.write(opened.content)
-    file.close()
-my_func(save="/home/kavalevskiyuri", link="https://github.com/rotorypower/lessons/blob/master/README.txt")
+
+def download_file(link, name):
+    r = requests.get(link)
+    open(name, "wb").write(r.content)
+
+
+download_file(
+    "https://raw.githubusercontent.com/rotorypower/lessons/master/LICENSE"
+)
