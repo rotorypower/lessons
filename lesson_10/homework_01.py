004787108c748d17c1084499d23d6aa49b24ac46
Kavalevski Yuri <kavalevskiyuri@gmail.com>
2021-12-19 21:40:27 +0300

    Add homework_10

--- lesson_10/homework_01.py
+++ lesson_10/homework_01.py
@@ -10,10 +10,9 @@ Square (атрибуты: две точки, методы: нахождение 
 import math
 
 class Point:
-    def __init__(self, x: int = 0 ,y: int = 0):
+    def __init__(self, x: int = 0, y: int = 0):
         self.x = x
         self.y = y
-
 
 
 class Figure:
@@ -23,52 +22,62 @@ class Figure:
         self.square = square
 
 
-
-        #Circle (атрибуты: координаты центра, длина радиуса; методы:нахождение периметра и площади окружности),
-class Circle:
+#Circle (атрибуты: координаты центра, длина радиуса; методы:нахождение периметра и площади окружности),
+class Circle(Figure):
     def __init__(self, center_circle, radius_length):
         self.center_circle = center_circle
         self.radius_length = radius_length
 
-    def area(self):
-        return 3.14 * self.radius_length
+    def area_circle(self):
+        return math.pi * self.radius_length
 
-    def perimetr(self):
-        return 2 * 3.14 * self.radius_length
+    def perimetr_circle(self):
+        return 2 * math.pi * self.radius_length
+
 
 #Triangle (атрибуты: три точки, методы: нахождение площади и периметра),
-class Triangle:
+class Triangle(Figure):
     def __init__(self, point_a, point_b, point_c):
         self.point_a = point_a
         self.point_b = point_b
         self.point_c = point_c
         self.p = (self.point_a + self.point_b + self.point_c) / 2
 
-    def areaTrian(self):
+    def perimetr_trian(self):  # периметр треугольника
+        return self.point_a + self.point_b + self.point_c
+
+    def area_trian(self):
         return math.sqrt(self.p * (self.p - self.point_a) * (self.p - self.point_b) * (self.p - self.point_c))
 
 
+#Square (атрибуты: две точки, методы: нахождение площади и периметра).
+class Square(Figure):
+    def __init__(self, side_a, side_b):
+        self.side_a = side_a
+        self.side_b = side_b
 
-    def perimetrTrian(self):     # периметр треугольника
-        return self.point_a + self.point_b + self.point_c
+    def area_tquaer(self):
+        return self.side_a * self.side_b
 
-
+    def perimetr_tquaer(self):
+        return (self.side_a + self.side_b) * 2
 
 
 
 
 if __name__ == "__main__":
 
+    my_list = [Circle(1, 4), Triangle(2, 2, 2), Square(2, 4)]
+    for area_figure in my_list:
+        if area_figure == my_list[0]:
+            print(f'Площадь круга: {area_figure.area_circle()}')
+        elif area_figure == my_list[1]:
+            print(f'Площадь треугольника: {area_figure.area_trian()}')
+        elif area_figure == my_list[2]:
+            print(f'Площадь квадрата: {area_figure.area_tquaer()}')
 
 
-    my_circle = Circle(0, 4)
-    my_triangle = Triangle(2,3,4)
 
-
-    print(my_circle.area())
-    print(my_circle.perimetr())
-    print(my_triangle.perimetrTrian())
-    print(my_triangle.areaTrian())
 
 
 
